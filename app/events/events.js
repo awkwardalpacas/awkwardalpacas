2feadc3d837c19cccfbad0e45c4c2cdbe0d64ec3
Adam <adam.l.guerra@gmail.com>
2015-04-10 14:13:14 -0500

    forgot to change from scope.event.url.

--- app/events/events.js
+++ app/events/events.js
@@ -9,12 +9,11 @@ angular.module('lunchCorgi.events', [])
 
 	$scope.addEvent = function() {
 		// check that all fields in the events.html form are filled out
-		if ($scope.event.url !== "" &&
-				$scope.event.location !== "" &&
+		if ($scope.event.location !== "" &&
 				$scope.event.date !== "" &&
 				$scope.event.time!== "" ) {
 					$scope.invalid = false
-					Events.addEvent($scope.event.url)
+					Events.addEvent($scope.event)
 					.then(function(newEvent) {
 						$scope.event = newEvent
 					})
