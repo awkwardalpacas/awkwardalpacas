558a93fd633f2019b3237823ee65ccfb8ccb1883
Josh Benson <bensonjosha@gmail.com>
2015-04-10 14:36:16 -0500

    small change to signup.js

--- app/signup/signup.js
+++ app/signup/signup.js
@@ -4,7 +4,7 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
+    templateUrl: 'signup/signup.html',
     controller: 'View1Ctrl'
   });
 }])
