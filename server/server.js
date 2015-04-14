d28c53695f67bbee5f365a30dbff3365cdccf5a3
Josh Benson <bensonjosha@gmail.com>
2015-04-14 18:58:49 -0500

    reverts server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var app = express();
 var port = /*process.env.PORT || */ 8000; //dynamic port assignment for deployment purposes
 
-app.use(express.static('..' + __dirname + '/public')); //serve assets with express
+app.use(express.static(__dirname)); //serve assets with express
 
 app.get("/", function (req, res) {
   res.redirect('index.html');
