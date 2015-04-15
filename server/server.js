cc24bfdec9616af871d79e582e61d565397fce0a
SFoskitt <stephanie.foskitt@gmail.com>
2015-04-15 17:19:02 -0500

    server.js serves index.html with 'node server.js' command

--- server/server.js
+++ server/server.js
@@ -2,14 +2,30 @@ var express = require('express');
 var mongoose = require('mongoose');
 
 var app = express();
-var port = /*process.env.PORT || */ 8000; //dynamic port assignment for deployment purposes
-
-app.use(express.static(__dirname)); //serve assets with express
+// var port = /*process.env.PORT || */ 8000; //dynamic port assignment for deployment purposes
+var __
+app.use(express.static(__dirname + '/../client/app')); 
+// app.use(express.static('client')); 
 
 app.get("/", function (req, res) {
-  res.redirect('index.html');
+  res.sendfile('index.html');
+  // res.redirect('index.html');
+  // res.redirect('../client/app/index.html');
+  // res.redirect('/../client/app/index.html');
+  // res.render('/../client/app/index');
+  // res.render('/../client/app/index.html');
+  // res.render('index.html');
+  // res.redirect('/client/app/index.html');
+  // res.send('Hello World!');
 });
 
 require('./middleware.js')(app, express);
 
-app.listen(port);
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
