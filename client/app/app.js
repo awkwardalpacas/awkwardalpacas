bdee17fb0e8d812401186d32cb7ad69232f60475
Josh Benson <bensonjosha@gmail.com>
2015-04-15 12:08:56 -0500

    changed module instantiation in app.js

--- client/app/app.js
+++ client/app/app.js
@@ -1,12 +1,16 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+var myApp = angular.module('myApp', [
   'ngRoute',
-  'myApp.signup',
-  'myApp.view2',
-  'myApp.version'
-]).
+  'myApp.signup'/*,
+  'myApp.events'*/
+  ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.
+      when('/signup', {
+        templateUrl: 'signup/signup.html',
+        controller: 'SignUpCtrl'
+    })/*.
+  $routeProvider.otherwise({redirectTo: '/signup'});*/
 }]);
