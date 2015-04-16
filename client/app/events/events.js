76f741e0fdeed83452ed018c05235204273b7c7a
SFoskitt <stephanie.foskitt@gmail.com>
2015-04-16 15:20:10 -0500

    adds 'joinEvent' function and button on template

--- client/app/events/events.js
+++ client/app/events/events.js
@@ -7,6 +7,24 @@ angular.module('lunchCorgi.events', [])
 	//if $scope.invalid is true, it will display an error message in the view
 	$scope.invalid = false
 
+	$scope.joinEvent = function() {
+		
+	// add companion function here for 
+	// add joinEvent function in Services.js
+	}
+
+  $scope.getLinks = function(){
+  	Links.getLinks($scope.data)
+  	  .then(function(data){
+  	  	$scope.data['links'] = data;
+  	  	console.log("links data is ", data);
+  	  })
+  	  .catch(function(error){
+  	  	console.error(error);
+  	  });
+  };	
+  $scope.getLinks();
+
 	$scope.addEvent = function() {
 		// check that all fields in the events.html form are filled out
 		if ($scope.event.description !== "" &&
@@ -21,19 +39,6 @@ angular.module('lunchCorgi.events', [])
 					$scope.invalid = true
 				}			
 	}
-/*
-COPIED FROM EVENT SCHEMA (server/events.js) FOR CONVENIENCE
-
-var EventSchema = new Schema ({
-	eventID : { type: Number, ref: 'eventID'},
-	description : String,
-	location : String,
-	datetime: Date,
-	creatorID : Number,
-	attendeeIDs : []
-});
-
-*/
 
 	// first page of events is page number 0; when more events are viewed, the page number is increased
 	$scope.pageNumber = 0
