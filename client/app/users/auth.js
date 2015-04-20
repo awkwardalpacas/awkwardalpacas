37fb86e7db7f269a1e605021713f0938aeaf44d2
Josh Benson <bensonjosha@gmail.com>
2015-04-19 20:50:40 -0500

    layout changes adds default date time

--- client/app/users/auth.js
+++ client/app/users/auth.js
@@ -21,7 +21,7 @@ angular.module('lunchCorgi.signup', ['ngRoute'])
       .then(function(token){
         $window.localStorage.setItem('com.corgi', token);
         $location.path('/');
-        })
+      })
       .catch(function(error){
         console.log(error);
       });
