3e4c5b480c99498c2e2baa74ceb873137295dd08
Pat Dalberg <pat.dalberg@gmail.com>
2015-04-21 15:09:46 -0500

    removes hardcoded texts and uses placeholders instead

--- client/app/events/events.js
+++ client/app/events/events.js
@@ -44,8 +44,8 @@ angular.module('lunchCorgi.events', [])
 
   $scope.initNewEventForm = function() {
     $scope.newEvent = {}
-    $scope.newEvent.description = 'Describe the event.'
-    $scope.newEvent.location = 'Where is the event?'
+    //$scope.newEvent.description = ''
+    //$scope.newEvent.location = ''
     $scope.newEvent.time = (new Date()).toTimeString().substr(0,5)
     $scope.newEvent.date = new Date(new Date() + new Date().getTimezoneOffset()*60000).toISOString().substr(0,10)    
   }
