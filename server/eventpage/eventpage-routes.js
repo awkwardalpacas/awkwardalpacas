14bbf284c1904e8f9c1705b5ac5aa7b38a54a91d
vishalatmakuri <findvishal@gmail.com>
2015-04-21 18:35:26 -0500

    modified the data entry

--- server/eventpage/eventpage-routes.js
+++ server/eventpage/eventpage-routes.js
@@ -1,14 +1,7 @@
 var eventsController= require('./eventpage-controller.js');
 
 module.exports = function(app){
-<<<<<<< HEAD
-
-  app.get('/',eventsController.getchats),
-  app.post('/chats',eventsController.postchats)
-  
-
-=======
   app.get('/chats',eventsController.getchats),
   app.post('/chats',eventsController.postchats)
->>>>>>> added chatroom for mongodb
+  
 }
