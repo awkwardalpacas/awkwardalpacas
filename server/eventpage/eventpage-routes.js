c0c64f96aa3823bdcb8781e50c291c50298fa5fc
vishalatmakuri <findvishal@gmail.com>
2015-04-21 16:38:52 -0500

    added chatroom for mongodb

--- server/eventpage/eventpage-routes.js
+++ server/eventpage/eventpage-routes.js
@@ -1,9 +1,14 @@
 var eventsController= require('./eventpage-controller.js');
 
 module.exports = function(app){
+<<<<<<< HEAD
 
   app.get('/',eventsController.getchats),
   app.post('/chats',eventsController.postchats)
   
 
+=======
+  app.get('/chats',eventsController.getchats),
+  app.post('/chats',eventsController.postchats)
+>>>>>>> added chatroom for mongodb
 }
