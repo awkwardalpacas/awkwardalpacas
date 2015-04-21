d888e752d3cf16fdb25bf8b3055155cdbab55ad3
vishalatmakuri <findvishal@gmail.com>
2015-04-21 16:57:18 -0500

    for rebase

--- server/eventpage/eventpage-controller.js
+++ server/eventpage/eventpage-controller.js
@@ -4,7 +4,7 @@ var jwt  = require('jwt-simple');
 
 var DB;
 
-mongo.connect('mongodb://heroku_app36102509:m3ei2epf1460981rpihk0egjsd@ds041377.mongolab.com:41377/heroku_app36102509', function(err, db) {
+mongo.connect('mongodb://localhost:27017/corgi', function(err, db) {
   if (err) throw err;
   // when the connection occurs, we store the connection 'object' (or whatever it is) in a global variable so we can use it elsewhere.
   DB = db
