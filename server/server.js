b6bfa081bc66629b9032acff30494e0e8a4417bf
vishalatmakuri <findvishal@gmail.com>
2015-04-22 11:22:14 -0500

    working on client

--- server/server.js
+++ server/server.js
@@ -18,7 +18,6 @@ var server = app.listen(process.env.PORT, function () {
 
   var host = server.address().address;
   var port = server.address().port;
-
   console.log('Example app listening at http://%s:%s', host, port);
 
 });
