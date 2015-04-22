fec21be67d02fb038f6c0d93d83cb08219ce92b3
vishalatmakuri <findvishal@gmail.com>
2015-04-22 18:56:16 -0500

    getting text on server

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 var app = express();
 // we just assign the port in the connection in each controller
-// var port = /*process.env.PORT || */ 8000; //dynamic port assignment for deployment purposes
+var port = /*process.env.PORT || */ 8000; //dynamic port assignment for deployment purposes
 var __
 app.use(express.static(__dirname + '/..'));
 
@@ -14,10 +14,11 @@ app.get("/", function (req, res) {
 
 require('./middleware.js')(app, express);
 
-var server = app.listen(process.env.PORT, function () {
+var server = app.listen(port)
+//   process.env.PORT, function () {
 
   var host = server.address().address;
-  var port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
+//   var port = server.address().port;
+//   console.log('Example app listening at http://%s:%s', host, port);
 
-});
+// });
