3b87993a01e1470dd3b0a64d1060c30fb2e42bed
Benita <benitazhang@gmail.com>
2015-04-23 13:48:09 -0500

    redirect fixed

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ var mongoose = require('mongoose');
 var path = require('path');
 var bodyParser  = require('body-parser');
 var DB;
-var plivo = require('plivo-node')
+var plivo = require('plivo-node');
+var CronJob = require('cron').CronJob;
 
 var api = plivo.RestAPI({
   authId: process.env.authId,
@@ -29,24 +30,36 @@ app.get("/", function (req, res) {
 app.post('/api/reminder', function(req,res){
 	var user = req.body.user
   var eventname=req.body.eventName
+  var time = req.body.eventTime.split("T")
+
 	console.log('user', user)
+  console.log('time', time)
+
+  // [2015-04-25][1800:00.000Z]
 
   var found = DB.collection('corgiuser').find({name:user})
   
   found.on('data', function(user){
-    var phoneNumber = user.phone;
+    var update = new CronJob('30 * * * * *',
+    function(){ console.log(user.phone) },
+    null,
+    true, // start now (when server spins up)
+    'America/Chicago'
+    );
+    // var phoneNumber = user.phone;
 
-    var params = {
-      'src': '19192751649', // Caller Id
-      'dst' : '1' + phoneNumber, // User Number to Call
-      'text' : "Hi, don't forget your event!"+eventname,
-      'type' : "sms"
-    };
+    // var params = {
+    //   'src': '19192751649', // Caller Id
+    //   'dst' : '1' + phoneNumber, // User Number to Call
+    //   'text' : "Hi, don't forget your event!"+eventname,
+    //   'type' : "sms"
+    // };
 
-    api.send_message(params, function (status, response) {
-      console.log('Status: ', status);
-      console.log('API Response:\n', response);
-    });
+    // api.send_message(params, function (status, response) {
+    //   console.log('Status: ', status);
+    //   console.log(params.dst)
+    //   console.log('API Response:\n', response);
+    // });
 
   })
 
